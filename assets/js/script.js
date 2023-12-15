32ec7c50725ff8144d22160aaa6203f551b517f6
Carlos Garita Campos <138426063+cgaritac@users.noreply.github.com>
2023-12-15 11:37:14 -0600

    Primera subida

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let navLinks = document.querySelectorAll('nav ul li a');
+// console.log(navLinks)
+
+navLinks.forEach((item)=>{
+    // console.log(item)
+    item.addEventListener('click', function(){
+        document.querySelector('nav ul li a.active').classList.remove('active')
+        item.classList.add('active')
+        document.querySelector('main > section.active').classList.remove('active')
+        document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active');
+        // console.log(`main > section${item.getAttribute('href')}`)
+    })
+})
+
+document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function(){
+    document.querySelector('#sidebar').classList.toggle('open')
+})
