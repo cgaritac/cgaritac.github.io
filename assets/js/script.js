300a0af95249393531c387bc5e68d55f70df6d25
Carlos Garita Campos <138426063+cgaritac@users.noreply.github.com>
2023-12-22 02:45:59 -0600

    About section responsive - skills-certifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-let navLinks = document.querySelectorAll('nav ul li a');
+let navLinks = document.querySelectorAll('a.inner-link');
 // console.log(navLinks)
 
 navLinks.forEach((item)=>{
     // console.log(item)
     item.addEventListener('click', function(){
         document.querySelector('nav ul li a.active').classList.remove('active')
-        item.classList.add('active')
+        document.querySelector(`nav ul li a[href='${item.getAttribute('href')}']`).classList.add('active')
+        // document.querySelector(`nav ul li a[href='#home']`).classList.add('active')
         document.querySelector('main > section.active').classList.remove('active')
         document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active');
         // console.log(`main > section${item.getAttribute('href')}`)
